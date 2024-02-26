2c2d61697fff3240cd9e7f3b79325f2066aa6ac4
S.Tougeiro <sidneytougeiro@gmail.com>
2024-02-26 17:49:49 -0300

    first commit

---
+++ src/CacheInterface.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Cache\Contract;
+
+    use Psr\SimpleCache\CacheInterface as PsrCacheInterface;
+
+
+    interface CacheInterface extends PsrCacheInterface
+    { }
