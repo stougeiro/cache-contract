4b029084e415d81a0bae0c5104cd08d55b156318
S.Tougeiro <sidneytougeiro@gmail.com>
2026-08-19 16:36:41 +0000

    Enhance CacheInterface with detailed PHPDoc comments and add phpstan configuration file

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -5,13 +5,34 @@
 
     interface CacheInterface
     {
+        /**
+         * @param string $key 
+         * @return bool 
+         */
         public function has(string $key): bool;
 
+        /**
+         * @param string $key 
+         * @param mixed $default 
+         * @return mixed 
+         */
         public function get(string $key, mixed $default = null): mixed;
 
+        /**
+         * @param string $key 
+         * @param mixed $value 
+         * @param int $ttl 
+         * @return bool 
+         */
         public function set(string $key, mixed $value, int $ttl = 300): bool;
 
+        /**
+         * @param string $key 
+         * @return bool 
+         */
         public function delete(string $key): bool;
 
+        /** @return bool 
+         */
         public function clear(): bool;
     }
